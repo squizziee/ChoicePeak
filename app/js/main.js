0f415e1184c372f18aa796687ab0a6c3e353156b
Ivan Liango <3925181@gmail.com>
2021-08-28 16:08:06 +0300

    Navbar ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,20 @@
- $(".search_toggle_btn").on("click", function() {
+ $("#search_toggle_btn").on("click", function() {
      $(this).addClass("hidden");
+     setTimeout(function() {
+        $(".logo").addClass("hidden");
+     }, 500);
      setTimeout(function() {
         $(".search_bar").removeClass("hidden");
      }, 750);
  })
+
+ $("#close_search_bar").on("click", function(event) {
+    event.preventDefault();
+    $(".search_bar").addClass("hidden");
+    setTimeout(function() {
+        $(".logo").removeClass("hidden");
+    }, 500);
+    setTimeout(function() {
+        $("#search_toggle_btn").removeClass("hidden");
+    }, 750);
+})
