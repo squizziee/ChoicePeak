764e6ec35fa7e367d0e2cbdc11b6f2323b6cf94a
Ivan Liango <3925181@gmail.com>
2021-08-31 13:34:45 +0300

    Post section ready

--- app/js/main.js
+++ app/js/main.js
@@ -23,3 +23,23 @@ $("#toggle_sidebar").on("click", function() {
     $("#sidebar").toggleClass("sidebar_active");
     $("#wrapper").toggleClass("wrapper_active");
 })
+
+var count = new Date("september 1, 2021 08:30:00").getTime();
+var x = setInterval(function(){
+    var now = new Date().getTime();
+        var d = count - now;
+
+        var days = Math.floor(d/(1000*60*60*24));
+        var hours = Math.floor((d%(1000*60*60*24))/(1000*60*60));
+        var minutes = Math.floor((d%(1000*60*60))/(1000*60));
+        var seconds = Math.floor((d%(1000*60))/1000);
+
+        document.getElementById("days").innerHTML = days + " days";
+        document.getElementById("hours").innerHTML = hours + " hours";
+        document.getElementById("minutes").innerHTML = minutes + " minutes";
+        document.getElementById("seconds").innerHTML = seconds + " seconds";
+
+        if (d <= 0) {
+            clearInterval(x);
+        }
+    },1000);
